c064816e024497e1c916da826be5f4889e62f787
Gilberson Campos <gra.campos@discente.ufma.br>
2025-01-19 20:05:44 -0300

    feat: remove image list from structure model

--- app/src/main/java/com/gilbersoncampos/relicregistry/data/model/PDFFormStructureModel.kt
+++ app/src/main/java/com/gilbersoncampos/relicregistry/data/model/PDFFormStructureModel.kt
@@ -1,8 +1,6 @@
 package com.gilbersoncampos.relicregistry.data.model
 
-import android.graphics.Bitmap
 import com.gilbersoncampos.relicregistry.data.enums.TypeFormPDF
-import org.w3c.dom.Text
 
-data class PDFFormStructureModel(val type:TypeFormPDF, val title:String, val options:List<OptionsPDFModel>?=null,val text:String?=null, val images:List<Bitmap>?=null)
+data class PDFFormStructureModel(val type:TypeFormPDF, val title:String, val options:List<OptionsPDFModel>?=null,val text:String?=null)
 data class OptionsPDFModel(val label:String, val selected:Boolean)
